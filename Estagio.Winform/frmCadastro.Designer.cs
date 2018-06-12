d7c96bb4c201f9ea7bff124ceb78f01ca54f7e5f
EM-Gustavo <gustavo.henrique@escolarmanager.com.br>
2018-06-12 11:12:26 -0300

    Criação de formulario base

---
+++ Estagio.Winform/frmCadastro.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Estagio.WinForms
+{
+    partial class frmCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.dgvProdutos = new System.Windows.Forms.DataGridView();
+            this.bsProdutos = new System.Windows.Forms.BindingSource(this.components);
+            this.txtInfoParaPesquisa = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsProdutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(3, 79);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(80, 32);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(3, 41);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(80, 32);
+            this.btnEditar.TabIndex = 1;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(3, 3);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(80, 32);
+            this.btnNovo.TabIndex = 0;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(3, 117);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(80, 32);
+            this.btnFechar.TabIndex = 1;
+            this.btnFechar.Text = "&Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Controls.Add(this.btnNovo);
+            this.flowLayoutPanel1.Controls.Add(this.btnEditar);
+            this.flowLayoutPanel1.Controls.Add(this.btnExcluir);
+            this.flowLayoutPanel1.Controls.Add(this.btnFechar);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(572, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(88, 455);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // dgvProdutos
+            // 
+            this.dgvProdutos.AutoGenerateColumns = false;
+            this.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdutos.DataSource = this.bsProdutos;
+            this.dgvProdutos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvProdutos.Location = new System.Drawing.Point(0, 26);
+            this.dgvProdutos.Name = "dgvProdutos";
+            this.dgvProdutos.Size = new System.Drawing.Size(572, 429);
+            this.dgvProdutos.TabIndex = 1;
+            this.dgvProdutos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProdutos_CellContentClick);
+            // 
+            // txtInfoParaPesquisa
+            // 
+            this.txtInfoParaPesquisa.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtInfoParaPesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInfoParaPesquisa.Location = new System.Drawing.Point(0, 0);
+            this.txtInfoParaPesquisa.Name = "txtInfoParaPesquisa";
+            this.txtInfoParaPesquisa.Size = new System.Drawing.Size(572, 26);
+            this.txtInfoParaPesquisa.TabIndex = 2;
+            this.txtInfoParaPesquisa.Text = "Informar as iniciais do nome";
+            this.txtInfoParaPesquisa.Enter += new System.EventHandler(this.txtInfoParaPesquisa_Enter);
+            this.txtInfoParaPesquisa.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInfoParaPesquisa_KeyDown);
+            this.txtInfoParaPesquisa.Leave += new System.EventHandler(this.txtInfoParaPesquisa_Leave);
+            // 
+            // frmCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 455);
+            this.Controls.Add(this.dgvProdutos);
+            this.Controls.Add(this.txtInfoParaPesquisa);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "frmCadastro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmCadastro_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsProdutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgvProdutos;
+        private System.Windows.Forms.TextBox txtInfoParaPesquisa;
+        private System.Windows.Forms.BindingSource bsProdutos;
+    }
+}
+
---
+++ Estagio.Winform/frmCadastroDeProduto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Estagio.Nucleo;
+using Estagio.Nucleo.Repositorio;
+
+
+namespace Estagio.Winform
+{
+    public partial class frmCadastroDeProduto : frmBaseAterrisagem
+    {
+        public frmCadastroDeProduto()
+        {
+            InitializeComponent();
+        }
+        private void frmCadastroDeProduto_Load(object sender, EventArgs e)
+        {
+            var produto = new Produto();
+            produto.Id = 1;
+            produto.Descricao = "refrigerante";
+            produto.PrecoUnitario = 4;
+            RepositorioDeProduto.Instancia.Add(produto);
+
+            var produto2 = new Produto();
+            produto2.Id = 2;
+            produto2.Descricao = "suco";
+            produto2.PrecoUnitario = 3;
+            RepositorioDeProduto.Instancia.Add(produto2);
+        }
+
+        protected override void RemovaItemDaLista(object itemSelecioando)
+        {
+            RepositorioDeProduto.Instancia.Delete((Produto)itemSelecioando);
+        }
+
+        protected override void EnviaItem(object itemSelecionado)
+        {
+            var frm = new frmEditarProduto();
+            frm.Produto = (Produto)itemSelecionado;
+        }
+
+        protected override Form CrieFormularioNovo()
+        {
+            return new frmNovoProduto();
+        }
+
+        protected override Form CrieFormularioEdicao()
+        {
+            return new frmEditarProduto();
+        }
+
+        protected override string ObtenhaMensagemBotaoOk()
+        {
+            return "Cadastrou Produto";
+        }
+
+        protected override DialogResult ExibaMensagemDeNaoSelecionado()
+        {
+            return MessageBox.Show("Selecione Produto", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        protected override IEnumerable<object> ObtenhaListaDeDados()
+        {
+            return RepositorioDeProduto.Instancia.GetAll();
+        }
+    }
+}
